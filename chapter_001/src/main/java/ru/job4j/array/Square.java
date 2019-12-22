b3f11b7826a8dac72a828ac865d2148e56dac66f
Andrey Gorbunov <gorbunovv.andrey@gmail.com>
2019-12-22 20:57:25 +0500

    6.0. Заполнить массив степенями чисел.[#207702]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        int index = 0;
+        for (int i = 1; i <= bound; i++) {
+            rst [index] = i * i;
+            index++;
+        }
+        return rst;
+    }
+}
