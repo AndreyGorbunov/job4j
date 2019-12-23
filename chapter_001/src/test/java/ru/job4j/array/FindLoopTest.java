f37129b1a53a5572603136fd5047318cc9d9501e
Andrey Gorbunov <gorbunovv.andrey@gmail.com>
2019-12-23 15:37:23 +0500

    6.5. Поиск индекса в диапазоне.[#207710]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -7,29 +7,31 @@ import static org.junit.Assert.assertThat;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0() {
-        FindLoop find = new FindLoop();
+    public void whenArrayHasLength5Then0() {
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int result = FindLoop.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
-    public void whenArrayHas4Then2() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {1, 5, 4, 8};
-        int value = 4;
-        int result = find.indexOf(input, value);
-        int expect = 2;
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
     @Test
-    public void whenArrayHas5ThenNoRst() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {1, 2, 3, 4};
+    public void whenNotFind() {
+        int[] input = new int[] {3, 2, 10, 2, 4};
         int value = 5;
-        int result = find.indexOf(input, value);
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
