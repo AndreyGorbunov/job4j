abff3feabe109da2451f2311013dba829c5a0f34
Andrey Gorbunov <gorbunovv.andrey@gmail.com>
2019-12-23 14:47:44 +0500

    6.4. Слово начинается с ...[#207705]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+        for (int index = 0; index <= pref.length - 1; index++) {
+            if (pref[index] != word[index]) {
+                result = false;
+                break;
+            } else result = true;
+        }
+        return result;
+    }
+}
