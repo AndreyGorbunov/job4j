4160a391a780579fc66d017d63bfa4354ca421ac
Andrey Gorbunov <gorbunovv.andrey@gmail.com>
2019-12-30 18:25:26 +0500

    6.7. Выигрышные комбинации в сокобан[#207707] исправлено

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,16 +3,17 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        for (int index = 0; index < board.length; index++) {
-            if (board[index][index] == 'X') {
-                for (int i = 0; i < board.length; i++) {
-                    if ((board[index][i] != board[index][index]) && (board[i][index] != board[index][index])) {
-                        result = false;
-                        break;
-                    } else {
-                        result = true;
-                    }
+        for (int row = 0; row < board.length; row++) {
+            int countRow = 0;
+            int countCell = 0;
+            for (int cell = 0; cell < board.length; cell++) {
+                if (board[row][row] == 'X') {
+                    countRow = board[row][row] == board[row][cell] ? countRow + 1 : countRow;
+                    countCell = board[row][row] == board[cell][row] ? countCell + 1 : countCell;
                 }
+            }
+            if (countRow == board.length || countCell == board.length) {
+                result = true;
             }
         }
         return result;
