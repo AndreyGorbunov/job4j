35efacf818648b80ecffa14e46d4fb984d03edf5
Andrey Gorbunov <gorbunovv.andrey@gmail.com>
2020-02-09 06:55:09 +0300

    2. Перегрузка конструктора.[#207858]

--- chapter_001/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Animal.java
@@ -1,10 +1,15 @@
 package ru.job4j.tracker;
 
 public class Animal {
+    public String name;
     public Animal() {
         System.out.println("Animal");
     }
+    public Animal(String name) {
+        this.name = name;
+        System.out.println(name);
+    }
     public static void main(String[] args) {
-        Animal Animal = new Animal();
+        Animal Animal = new Animal("Animal");
     }
 }
--- chapter_001/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_001/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,10 +1,15 @@
 package ru.job4j.tracker;
 
 public class Tiger extends Predator {
+    public String name;
     public Tiger() {
         System.out.println("Tiger");
     }
+    public Tiger(String name) {
+        this.name = name;
+        System.out.println(name);
+    }
     public static void main(String[] args) {
-        Tiger Tiger = new Tiger();
+        Tiger Tiger = new Tiger("Tiger");
     }
 }
