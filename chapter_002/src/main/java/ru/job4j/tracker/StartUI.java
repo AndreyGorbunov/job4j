bb4fd0234d1e2cacb2920878e8b6dae9cd251e4f
Andrey Gorbunov <gorbunovv.andrey@gmail.com>
2020-09-24 13:42:50 +0300

    5. Реализовать класс Tracker[396#207868]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+
+        Item item1 = new Item();
+        item1.setName("ItemOne");
+        Item item2 = new Item();
+        item2.setName("ItemTwo");
+
+        tracker.add(item1);
+        tracker.add(item2);
+
+        System.out.println(tracker.findById(1));
+    }
+
+}
--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,55 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[size];
+        int sizeNotNull = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemsWithoutNull[sizeNotNull] = item;
+                sizeNotNull++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, sizeNotNull);
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsName = new Item[size];
+        int sizeName = 0;
+            for (int i = 0; i < size; i++) {
+            Item names = items[i];
+            if (items[i].getName().equals(key)) {
+                itemsName[sizeName] = names;
+                sizeName++;
+            }
+        }
+        itemsName = Arrays.copyOf(itemsName, sizeName);
+            return itemsName;
+    }
+
+    public Item findById(int id) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
